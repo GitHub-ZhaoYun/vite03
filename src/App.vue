eb9daa2c4e689fd2605b78be14968edc8cca8573
zhaoyun <13521701468@163.com>
2023-02-07 14:29:18 +0800

    国际化-终版

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,12 @@
     </el-config-provider>
 </template>
 <script setup>
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
-import en from 'element-plus/dist/locale/en.mjs'
-import { useStore } from './stores/language'
-const store = useStore();
-let { currentLanguage } = storeToRefs(store);
-let locale = computed(() => (currentLanguage.value === 'zh' ? zhCn : en))
+    import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+    import en from 'element-plus/dist/locale/en.mjs'
+    import { useStore } from './stores/language'
+    const store = useStore();
+    let { currentLanguage } = storeToRefs(store);
+    let locale = computed(() => (currentLanguage.value === 'zh' ? zhCn : en))
 </script>
 <style>
 @import "./assets/css/reset.css";
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,12 @@
 <template>
     <div>
         <Header></Header>
-        <div style="width: 30%;">
-            <el-table mb-1 :data="[]" />
-            <el-pagination :total="100" />
+        <div style="width: 100%;">
+            <div style="width: 1200px; margin: 30px auto;">
+                <el-table mb-1 :data="[]" />
+                <el-pagination :total="100" />
+            </div>
+
         </div>
     </div>
 </template>
